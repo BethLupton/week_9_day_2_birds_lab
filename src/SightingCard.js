2396d24752b786edd13a6ff0419addfb6d8f79e2
Beth Lupton <luptonbeth21@gmail.com>
2023-05-16 15:48:20 +0100

    First Commit

---
+++ src/SightingCard.js
@@ -0,0 +1,43 @@
+import { deleteSighting } from "./SightingService"
+import UpdateForm from "./UpdateForm"
+import React, {useState} from 'react';
+
+
+const SightingCard = ({sighting, removeSighting, updateSighting}) => {
+
+    console.log(sighting);
+    const handleDelete = () => {
+        deleteSighting(sighting._id).then(()=>{
+            removeSighting(sighting._id);
+        })
+    }
+
+    const [isClicked, setClicked] = useState(false);
+
+    const handleClick = () => {
+        setClicked(!isClicked);
+    };
+
+    // const handleUpdate = () => {
+    //     updateSighting(sighting._id)
+    // }
+
+    return (
+        <>
+        <div>
+            <UpdateForm/>
+        </div>
+        <div>
+            <h1>{sighting.species}</h1>
+            <p>Location: {sighting.location}</p>
+            <p>Date: {sighting.date}</p>
+            <button onClick={handleDelete}> 🗑 </button>
+            <button onClick={handleClick}> ✏️ </button>
+            <hr></hr>
+        </div>
+        </>
+    )
+
+}
+
+export default SightingCard;
